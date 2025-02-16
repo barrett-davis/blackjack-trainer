ac85d081d59e06f5345088ad4379d09e6dd68a42
barrett-davis <davis.barrett@protonmail.com>
2025-02-16 16:06:39 -0600

    Added the ability to split. Improved flow of output text. Made some repeating code into functions.

--- blackjacktrainer.py
+++ blackjacktrainer.py
@@ -1,6 +1,10 @@
 import random
 import time
 
+def print_wait(string):
+    print(string)
+    time.sleep(1)
+
 def main():
     while True:
         play_game()
@@ -8,15 +12,15 @@ def main():
             restart_game = input("Would you like to play again? (y/n): ").lower()
             if restart_game == 'y':
                 break
-            elif (restart_game == 'n'):
-                print("Thanks for playing!")
+            elif (restart_game == 'n'): # if player enters "n", end the game
+                print_wait("Thanks for playing!")
                 return
             else:
-                print("Invalid input. Please enter 'y' or 'n'.")
+                print_wait("Invalid input. Please enter 'y' or 'n'.")
 
 
 
-
+# draw a card from the deck and assign a value to face/ace
 def draw_card():
     card_int = random.randint(1, 13)
 
@@ -41,10 +45,12 @@ def draw_card():
             card_label = str(card_value)
 
     return card_label, card_value
+    # return 'J', 10 # DEBUG - always draw J
 
+# calculate the point value of the hand
 def calculate_hand(hand):
     total = sum(card[1] for card in hand)
-    num_aces = sum(1 for card in hand if "A" in card[0])
+    num_aces = sum(1 for card in hand if "A" in card[0]) # check for Aces to consider 1/11 assignment
 
     while (total > 21 and num_aces > 0):
         total -= 10
@@ -52,14 +58,75 @@ def calculate_hand(hand):
 
     return total
 
-
+# print_wait the hand and (optional) point value
 def display_hand(name, hand, total = None, hide_second_card = False):
     if (hide_second_card):
-        print(f"{name}: ['{hand[0][0]}', '??'] {hand[0][1]}")
+        print(f"{name}: ['{hand[0][0]}', '??']")
     else:
-        print(f"{name}: {str([card[0] for card in hand])} {total}")
+        print_wait(f"{name}: {str([card[0] for card in hand])} {total}")
+            
+# player turn to hit or stand
+def player_turn(player_hand, dealer_hand):
+    player_total = calculate_hand(player_hand)
+
+    while (player_total < 21):
+        action = input("Would you like to hit (h) or stand (s)? ").lower()
+
+        if (action == 'h'):
+            print_wait("Player hits.")
+            player_hand.append(draw_card())
+            player_total = calculate_hand(player_hand)
+            display_hand("Dealer", dealer_hand, hide_second_card=True)
+            display_hand("Player", player_hand, player_total)
+
+            if player_total > 21:
+                print_wait("Player busts.")
+                return
+            
+            if player_total == 21:
+                print_wait("Player has 21")
+                return
+
+        elif (action == 's'):
+            print_wait("Player stands.")
+            break
+
+        else:
+            print_wait("Invalid input. Please enter 'h' or 's'.")
+
+def dealer_turn(dealer_hand):
+    # dealer draws while below 17
+    dealer_total = calculate_hand(dealer_hand)
+    while (dealer_total < 17):
+        dealer_hand.append(draw_card())
+        dealer_total = calculate_hand(dealer_hand)
+        display_hand("Dealer", dealer_hand, dealer_total)
+        if (dealer_total > 21):
+            print_wait(f"Dealer busts.")
+            return # end game if dealer busts
+        if (dealer_total >= 17):
+            print_wait(f"Dealer stands.")
+        
+        
+# display the final results
+def results(hand_name, player_hand, dealer_hand):
+    player_total = calculate_hand(player_hand)
+    dealer_total = calculate_hand(dealer_hand)
+
+    print_wait(f"{hand_name} Results:")
+    display_hand("Dealer", dealer_hand, dealer_total, hide_second_card=False)
+    display_hand("Player", player_hand, player_total)
+
+    if ((player_total <= 21) and ((dealer_total > 21) or (player_total > dealer_total))):
+        print_wait("Player wins. Congratulations!")
+    elif (player_total == dealer_total):
+        print_wait("It's a tie.")
+    else:
+        print_wait("Dealer wins.")
+
 
 def play_game():
+    # create the dealer and player hands and draw two cards for each
     dealer_hand = []
     player_hand = []
 
@@ -68,52 +135,49 @@ def play_game():
     player_total = calculate_hand(player_hand)
 
     dealer_hand.append(draw_card())
-    dealer_total = calculate_hand(dealer_hand)
+    dealer_hand.append(draw_card())
 
-    display_hand("Dealer", dealer_hand, dealer_total, hide_second_card=True)
+    display_hand("Dealer", dealer_hand, hide_second_card=True)
     display_hand("Player", player_hand, player_total)
 
-
+    # check for blackjack
     if (player_total == 21):
-        print("Blackjack! You win!\n")
+        print_wait("Blackjack! You win!")
         return
+    
+    # check for split
+    if (player_hand[0][0] == player_hand[1][0]):
+        while True:
+            action = input(f"Would you like to split? (y/n) ").lower()
+            if (action == 'y'):
+                print_wait(f"Player splits.")
+                hand1 = [player_hand[0], draw_card()]
+                hand2 = [player_hand[1], draw_card()]
 
-    while (player_total < 21):
-        action = input("Would you like to hit (h) or stand (s)? ").lower()
-        if (action == 'h'):
-            print("Player hits.\n")
-            time.sleep(1)
-            player_hand.append(draw_card())
-            player_total = calculate_hand(player_hand)
-            display_hand("Dealer", dealer_hand, dealer_total, hide_second_card=True)
-            display_hand("Player", player_hand, player_total)
-            print("")
-            time.sleep(1)
-            if player_total > 21:
-                print("Player busts.\n")
+                display_hand("Dealer", dealer_hand, hide_second_card=True)
+                display_hand("Player H1", hand1, calculate_hand(hand1))
+                player_turn(hand1, dealer_hand)
+
+                display_hand("Dealer", dealer_hand, hide_second_card=True)
+                display_hand("Player H2", hand2, calculate_hand(hand2))
+                player_turn(hand2, dealer_hand)
+
+                dealer_turn(dealer_hand)
+
+                results("Player H1", hand1, dealer_hand)
+                results("Player H2", hand2, dealer_hand)
+
+                return
+
+            elif (action == 'n'):
                 break
-        elif (action == 's'):
-            print("Player stands.\n")
-            break
-        else:
-            print("Invalid input. Please enter 'h' or 's'.\n")
 
-    while (dealer_total < 17):
-        dealer_hand.append(draw_card())
-        dealer_total = calculate_hand(dealer_hand)
-        display_hand("Dealer", dealer_hand, dealer_total)
-        display_hand("Player", player_hand, player_total)
-        print("")
-        time.sleep(1)
-        if (dealer_total > 21):
-            print(f"Dealer busts.")
+            else:
+                print_wait("Invalid input. Please enter 'y' or 'n'.")
 
-    if ((dealer_total > 21) or (player_total > dealer_total and player_total <= 21)):
-        print("Player wins. Congratulations!\n")
-    elif (player_total == dealer_total):
-        print("It's a tie.\n")
     else:
-        print("Dealer wins.\n")
-
+        player_turn(player_hand, dealer_hand)
+        dealer_turn(dealer_hand)
+        results("Player H1", player_hand, dealer_hand)
 
 main()
